3d5593517d2f66f8dc105833ab6859b0ebb2c7d6
pulkitahuja <pulkit3023@gmail.com>
2019-02-07 15:53:21 +0530

    color changes

---
+++ typ.js
@@ -0,0 +1,33 @@
+var s = ['Designer.', 'Developer.', 'Creater.'];
+function typejs(stringarr) {
+    
+    var ele = document.getElementById('st');
+    var i = 0;
+    var j = 0;
+    var x = [];
+    var flag = 0;
+
+    setInterval(
+        function () {
+            if (j == s.length) {
+                j = 0;
+            }
+            if (i < s[j].length && flag == 0) {
+                x.push(s[j][i]);
+                i++;
+            }
+            else {
+                {x.pop();
+                i--;
+                flag = 1;
+                if (x.length == 0) {
+                    flag = 0;
+                    j++;
+                }}
+            }
+            ele.innerHTML=''+ x.join('');
+            // ele.style.borderRight='1px';
+        }, 150
+    )
+}
+typejs(s);
