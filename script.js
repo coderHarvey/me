c0fcb9fc0379caaeb551c5e33b0df0548433a247
pulkitahuja <pulkit3023@gmail.com>
2019-03-10 22:17:05 +0530

    modal bug fixes

--- script.js
+++ script.js
@@ -73,7 +73,7 @@ function currentSlides(n){
 function showSlides(n) {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-
+    console.log(slides);
     if (n > slides.length) {slideIndex = 1}
     if (n < 1) {slideIndex = slides.length}
     for (i = 0; i < slides.length; i++) {
