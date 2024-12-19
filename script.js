40a3910f57d3804e08ada9d9a1e8fe442a3e2902
pulkitahuja <pulkit3023@gmail.com>
2024-12-20 00:29:20 +0530

    Revamped website

--- script.js
+++ script.js
@@ -1,92 +1,31 @@
-var button= document.getElementById('disButton');
-var list=document.getElementById('list');
+const openNav = document.getElementById("openNav");
+const closeNav = document.getElementById("closeNav");
+const sideNav = document.getElementById("sideNav");
+const contact = document.getElementById("contact");
+const contactCaller = document.getElementById("contactCaller");
+const aboutMeCaller = document.getElementById("aboutMeCaller");
+const experienceCaller = document.getElementById("experienceCaller");
 
-function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    list.style.display="inherit";
-  }
-  
-  /* Set the width of the side navigation to 0 */
-  function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-  }
+aboutMeCaller.addEventListener("click", () => {
+    sideNav.classList.add("translate-x-full");
+});
 
-function hide(tagname){
-var btn=event.target;
-console.log(btn);
-var tg=tagname;
-var el=document.getElementById(tg);
-el.style.display= (el.style.display=="" ? "block": "");
-btn.innerHTML= el.style.display==="block" ? "-": "+";
-}
-function changeColor(color){
-    document.body.style.background="#"+color;
-    // setTimeout(()=>{
-    // document.body.style.background="-webkit-gradient(linear, left top, left bottom, from(rgb(41, 29, 48)), to(#0f0b0f)) fixed";
-    // },5000);
-}
+experienceCaller.addEventListener("click", () => {
+    sideNav.classList.add("translate-x-full");
+});
 
-function displayList(){
-    if(list.style.display=="inherit") 
-    {
-        list.style.display="none";
-    
-    }
-    else{
-        list.style.display="inherit";
+contactCaller.addEventListener("click", () => {
+    contact.classList.add("animate-pulse");
+    sideNav.classList.add("translate-x-full");
+    setTimeout(()=> {
+        contact.classList.remove("animate-pulse");
+    }, 6000)
+});
 
-    }
-    if(button.style.fontSize=="15px") 
-    {
-        button.style.fontSize="25px";
-    
-    }
-    else{
-        button.style.fontSize="15px";
+openNav.addEventListener("click", () => {
+    sideNav.classList.remove("translate-x-full");
+});
 
-    }
-   
-}
-
-var modal= document.getElementById('myModal');
-
-function openModal() {
-    modal.style.display= "block";
-
-}
-
-function closeModal(){
-    modal.style.display ="none"; 
-}
-
-var slideIndex= 1;
-showSlides(slideIndex);
-
-function plusSlides(n) {
-    showSlides(slideIndex +=n);
-}
-
-function currentSlides(n){
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
-    console.log(slides);
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-    }
-    
-    slides[slideIndex-1].style.display = "block";
- 
-  }
-
-
-
-var ele=this.document.getElementsByClassName("skills");
-function enter(){
-ele.classList.add('entered');
-}
+closeNav.addEventListener("click", () => {
+    sideNav.classList.add("translate-x-full");
+});
